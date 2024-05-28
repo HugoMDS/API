99f3e201b3421f22d5d63f2e3eb7e688e50db65d
Hugo Chamard <hugochamard@MacBook-Air-de-hugo.local>
2024-05-28 10:43:04 +0200

    added debug

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, jsonify
+import logging
 
 app = Flask(__name__)
 
+logging.basicConfig(level=logging.DEBUG)
+
 @app.route('/')
 def home():
+    app.logger.debug("Home route accessed")
     return "Bienvenue à mon API Flask!"
 
 @app.route('/api/data', methods=['GET'])
 def get_data():
+    app.logger.debug("Data route accessed")
     data = {
         "message": "Voici vos données",
         "data": [1, 2, 3, 4, 5]
@@ -15,6 +20,5 @@ def get_data():
     return jsonify(data)
 
 if __name__ == '__main__':
+    app.logger.debug("Starting the Flask app")
     app.run(host='0.0.0.0', port=5000)
-
-# Commentaire pour forcer un redéploiement
