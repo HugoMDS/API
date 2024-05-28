8af994f9b54d057ee8f251733a728e3eae9ed3d1
Hugo Chamard <hugochamard@MacBook-Air-de-hugo.local>
2024-05-28 10:05:26 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bienvenue à mon API Flask!"
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {
+        "message": "Voici vos données",
+        "data": [1, 2, 3, 4, 5]
+    }
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
