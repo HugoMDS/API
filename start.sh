307d155b835c1cdd1de02dab77bf2dcf37cec2c3
Hugo Chamard <hugochamard@MacBook-Air-de-hugo.local>
2024-05-28 11:24:17 +0200

    encore bug fix

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Installer les dépendances
+pip3 install --upgrade pip
 pip3 install -r requirements.txt
 
 # Démarrer l'application Flask
-gunicorn app:app
+exec gunicorn app:app --bind 0.0.0.0:5000 --workers 3
