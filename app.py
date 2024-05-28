70048148979ae3a42df26d5a85a2303f66c5df7b
Hugo Chamard <hugochamard@MacBook-Air-de-hugo.local>
2024-05-28 12:39:54 +0200

    test

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 import logging
 import json
 from scripts.recup_urls import recup_urls
-from scripts.test import process_csv
+from scripts.analyse_seo import process_csv
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
@@ -13,13 +13,13 @@ def home():
     return "Bienvenue à mon API Flask!"
 
 @app.route('/api/search', methods=['GET'])
-def get_urls():
-    app.logger.debug("recup_urls route accessed")
-    domain = request.args.get('domaine')
-    if not domain:
-        return jsonify({"error": "Missing query parameter 'domaine'"}), 400
-    result = recup_urls(domain)
-    return jsonify(result)
+def search_google():
+    app.logger.debug("serp route accessed")
+    query = request.args.get('query')
+    if not query:
+        return jsonify({"error": "Missing query parameter 'query'"}), 400
+    results = scrape_google(query)
+    return jsonify(results)
 
 @app.route('/api/recup_urls', methods=['GET'])
 def get_urls():
