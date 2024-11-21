d153135a8bff2896d2b19b04f27b4209d3721b74
Hugo Chamard <hugochamard@MacBook-Air-de-hugo.local>
2024-11-21 20:37:44 +0100

    pdf from html

--- scripts/generate_pdf.py
+++ scripts/generate_pdf.py
@@ -1,16 +1,11 @@
 from io import BytesIO
-import pdfkit
+from weasyprint import HTML
 
 def generate_pdf_from_html(html_content):
     """
     Génère un fichier PDF à partir de HTML et retourne un flux binaire.
     """
-    options = {
-        'page-size': 'A4',
-        'encoding': 'UTF-8',
-        'quiet': ''
-    }
     pdf_buffer = BytesIO()
-    pdfkit.from_string(html_content, pdf_buffer, options=options)
+    HTML(string=html_content).write_pdf(pdf_buffer)
     pdf_buffer.seek(0)
     return pdf_buffer
